8fe2c95a73d51ee6f1a9bfd3e789005b71b7fe54
cbogues <YOUR-EMAIL-ADDRESS>
2015-01-23 17:20:54 -0800

    child model added

---
+++ spec/factories/children.rb
@@ -0,0 +1,14 @@
+FactoryGirl.define do
+  factory :child do
+    name "MyString"
+age_yrs 1
+age_months 1
+gender "MyString"
+style 1
+shirt_size 1
+pant_size 1
+jacket_size 1
+shoe_size 1
+  end
+
+end
