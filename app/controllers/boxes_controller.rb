a24a39c1c870d63ad216c9db15188bf4d0991b22
coloradojay <delarosa.j@gmail.com>
2015-01-27 21:29:54 -0800

    Updating Box Controller

--- app/controllers/boxes_controller.rb
+++ app/controllers/boxes_controller.rb
@@ -5,19 +5,40 @@ class BoxesController < ApplicationController
 	end
 
 	def create
+		@box = Box.new(box_params)
 
+		if @box.save
+			redirect_to boxes_path
+		else
+			#if unsuccessful, reset to the new page
+			render "new"
+		end
 	end
 
 	def show
-
+  	@box = Box.find(params[:id])
 	end
 
 	def update
+		@box = Post.find(params[:id])
 
+		if @box.update_attributes(box_params)
+			redirect_to boxes_path
+		else
+			#if unsuccessful, show to the edit page
+			render :update
+		end
 	end
 
 	def destroy
+		@post = Post.find(params[:id])
+		@post.destroy
+		redirect_to posts_path
+	end
 
+	private
+	def box_params
+		params.require(:box).permit(:order_number,:status,:user_id,:child_id)
 	end
 
 end
