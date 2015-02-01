f4868699409cb4e0b0aa7a546bc745fed98d291d
coloradojay <delarosa.j@gmail.com>
2015-02-01 15:22:31 -0800

    Adding Filters page

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,13 +2,17 @@ class ProductsController < ApplicationController
   require_relative "ShopStyleAPI.rb"
   require_relative "GirlsShopStyleAPI.rb"
 
+  GENDER = ["boy","girl"]
+  STYLE = ["athletic","formal","everyday","trendy"]
+  SIZE = ["0-3mo","6-9mo","9-12mo","12-18mo","2T","3T","4T","5T"]
+
 	def index
 
 	end 
 
 	def new
 		call_api = GirlsShopStyleAPI.new
-
+    @product = Product.new
     @jacket_products  = call_api.jackets_API_data
     @shirt_products   = call_api.shirts_API_data
     @bottom_products  = call_api.bottoms_API_data
---
+++ config/initializers/constants.rb
@@ -0,0 +1,3 @@
+  GENDER = ["boy","girl"]
+  STYLE = ["athletic","formal","everyday","trendy"]
+  SIZE = ["0-3mo","6-9mo","9-12mo","12-18mo","2T","3T","4T","5T"]
