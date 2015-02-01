72ba9b573cb721ee0299b7f76d5ef7f894a8b882
jakoko <aerisgurlie777@gmail.com>
2015-02-01 12:05:04 -0800

    added some to boxes controller create action

--- app/controllers/boxes_controller.rb
+++ app/controllers/boxes_controller.rb
@@ -15,8 +15,33 @@ class BoxesController < ApplicationController
 	end
 
 	def create
-		@box = Box.new(box_params)
+		@box = Box.new
+
 		@box.user = current_user
+		@box.order_number = Box.count.to_s
+		# @box.status = BOX_STATUSES[0]
+
+		###############
+		# Not done yet!
+		###############
+
+		# Save products to Products model
+		box_params[:products_id].each { |type, id|
+			if (type == :shirt_id) && !id.nil?
+				product = ShopStyleAPI.product_info(id)
+				# include shirt size
+				@box.products.new(name: product["name"], price: product["priceLabel"])
+
+			elsif (type == :pant_id) && !id.nil?
+
+
+			elsif (type == :jacket_id) && !id.nil?
+				
+
+			end
+		}
+
+				
 		if @box.save
 			redirect_to boxes_path
 		else
@@ -48,7 +73,7 @@ class BoxesController < ApplicationController
 
 	private
 	def box_params
-		params.require(:box).permit(:order_number,:status,:user_id,:child_id,:gender,:shirt_size,:pant_size,:jacket_size,:style)
+		params.require(:box).permit(:gender,:shirt_size,:pant_size,:jacket_size,:style, :products_id => [:shirt_id,:pant_id,:jacket_id])
 	end
 
 end
