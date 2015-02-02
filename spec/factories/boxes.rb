43925c3e2744cb541e92516556902ef0de811aea
coloradojay <delarosa.j@gmail.com>
2015-02-02 08:14:39 -0800

    Box spec adjustment

--- spec/factories/boxes.rb
+++ spec/factories/boxes.rb
@@ -3,7 +3,6 @@ FactoryGirl.define do
     order_number "01262014173700"
 		status 1
 		user_id 1
-		child_id 1
   end
 
 end
--- spec/models/box_spec.rb
+++ spec/models/box_spec.rb
@@ -6,16 +6,6 @@ RSpec.describe Box, :type => :model do
 		expect(box).to be_invalid
 	end
 
-  it "is invalid without a child" do
-  	box = FactoryGirl.build(:box, child_id: nil)
-  	expect(box).to be_invalid
-  end
-
-  it "is invalid without an order number" do
-  	box = FactoryGirl.build(:box, order_number: nil)
-  	expect(box).to be_invalid
-  end
-
   it "is invalid without a status" do
   	box = FactoryGirl.build(:box, status: nil)
   	expect(box).to be_invalid
